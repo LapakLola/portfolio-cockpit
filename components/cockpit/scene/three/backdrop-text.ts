b1c1a69758000c60e83c449a47c8b51dbb6d675e
LapakLola <tarunkosta007@gmail.com>
2026-07-17 19:21:19 +0530

    Update backdrop-text.ts

--- components/cockpit/scene/three/backdrop-text.ts
+++ components/cockpit/scene/three/backdrop-text.ts
@@ -28,7 +28,7 @@ const font: Font = new FontLoader().parse(
 export function createBackdropText(
   scene: THREE.Scene,
   renderer: THREE.WebGLRenderer,
-  { text = 'MATTEO DANTE', position, size = 16, depth = 2 }: Options = {}
+  { text = 'PYASII JIIII', position, size = 16, depth = 2 }: Options = {}
 ): BackdropTextBundle {
   const geo = new TextGeometry(text, {
     font,
